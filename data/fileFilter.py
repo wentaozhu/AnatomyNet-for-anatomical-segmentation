4ebc258390d207da9753cec62f0846662d039fad
Wentao Zhu <wentaozhu1991@gmail.com>
2018-12-28 17:58:40 -0800

    upload data
    upload data

--- data/fileFilter.py
+++ data/fileFilter.py
@@ -1,1 +1,10 @@
-
+import os
+pathList = ["./HNPETCTclean/", "./HNCetuximabclean/", "./pddca18/"]
+for path in pathList:
+    for subPath in os.listdir(path):
+        for f in os.listdir(path+subPath):
+            if f.endswith("_crp.npy"):
+                os.remove(path+subPath+"/"+f)
+        for f in os.listdir(path+subPath+"/structures/"):
+            if f.endswith("_crp.npy"):
+                os.remove(path+subPath+"/structures/"+f)
